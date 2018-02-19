f94ac3939109a345d6578890a56124cfa2c45f40
ErnestMazurin <chegevara001@gmail.com>
2018-02-20 02:01:25 +0300

    add help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,9 @@
 #!/usr/bin/env node
+import program from 'commander';
 
-console.log('I`m OK!');
+program
+  .version('0.0.6')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
