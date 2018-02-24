626c74b5429847690d94231b2375d6fd3bb7e87e
ErnestMazurin <chegevara001@gmail.com>
2018-02-24 10:02:33 +0300

    little refactoring

--- __tests__/json.test.js
+++ __tests__/json.test.js
@@ -1,17 +1,19 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const expResPath = '__tests__/__fixtures__/expectedResult';
-const expectedResult = fs.readFileSync(expResPath).toString();
+describe('Flat config files', () => {
+  const expResPath = '__tests__/__fixtures__/expectedResult';
+  const expectedResult = fs.readFileSync(expResPath).toString();
 
-test('1. flat json', () => {
-  expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json', 'json')).toBe(expectedResult);
-});
+  test('json', () => {
+    expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json', 'json')).toBe(expectedResult);
+  });
 
-test('2. flat yaml', () => {
-  expect(genDiff('__tests__/__fixtures__/before.yaml', '__tests__/__fixtures__/after.yaml', 'yaml')).toBe(expectedResult);
-});
+  test('yaml', () => {
+    expect(genDiff('__tests__/__fixtures__/before.yaml', '__tests__/__fixtures__/after.yaml', 'yaml')).toBe(expectedResult);
+  });
 
-test('3. flat ini', () => {
-  expect(genDiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini', 'ini')).toBe(expectedResult);
+  test('ini', () => {
+    expect(genDiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini', 'ini')).toBe(expectedResult);
+  });
 });
--- src/index.js
+++ src/index.js
@@ -14,19 +14,17 @@ const render = (differenceList) => {
   return `{\n${core}}\n`;
 };
 
-const diff = (config1, config2) => {
-  const keys1 = new Set(_.keys(config1));
-  const keys2 = new Set(_.keys(config2));
+const getDiff = (config1, config2) => {
   const uniqKeys = _.union(_.keys(config1), _.keys(config2));
 
   const callback = (acc, key) => {
     if (config1[key] === config2[key]) {
       return [...acc, { sign: ' ', key, value: config1[key] }];
     }
-    if (keys1.has(key) && keys2.has(key)) {
+    if (_.has(config1, key) && _.has(config2, key)) {
       return [...acc, { sign: '+', key, value: config2[key] }, { sign: '-', key, value: config1[key] }];
     }
-    if (keys1.has(key) && !keys2.has(key)) {
+    if (_.has(config1, key) && !_.has(config2, key)) {
       return [...acc, { sign: '-', key, value: config1[key] }];
     }
     return [...acc, { sign: '+', key, value: config2[key] }];
@@ -36,13 +34,13 @@ const diff = (config1, config2) => {
 };
 
 export default (path1, path2, format = 'json') => {
-  const file1 = fs.readFileSync(path1).toString();
-  const file2 = fs.readFileSync(path2).toString();
+  const fileContent1 = fs.readFileSync(path1).toString();
+  const fileContent2 = fs.readFileSync(path2).toString();
 
-  const config1 = parseDispatcher[format](file1);
-  const config2 = parseDispatcher[format](file2);
+  const config1 = parseDispatcher[format](fileContent1);
+  const config2 = parseDispatcher[format](fileContent2);
 
-  const differenceList = diff(config1, config2);
+  const differenceList = getDiff(config1, config2);
 
   return render(differenceList);
 };
