1bfe4c954a295911edbe446d9a8fd6e850901aff
ErnestMazurin <chegevara001@gmail.com>
2019-12-12 22:47:51 +0300

    refactored tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,64 +1,41 @@
 import fs from 'fs';
 import genDiff from '../src';
+import 'regenerator-runtime/runtime';
 
 const getPath = (name) => `__tests__/__fixtures__/${name}`;
 
-const testGenDiff = (path1, path2, expected, format) => {
-  expect(genDiff(getPath(path1), getPath(path2), format)).toBe(expected);
-};
-
 const read = (path) => fs.readFileSync(getPath(path)).toString();
 
-const testSet1 = [
-  ['before.json', 'after.json'],
-  ['before.yaml', 'after.yaml'],
-  ['before.ini', 'after.ini'],
+const testSet = [
+  // Complex output with flat config files
+  ['before.json', 'after.json', undefined, 'expectedResult'],
+  ['before.yaml', 'after.yaml', undefined, 'expectedResult'],
+  ['before.ini', 'after.ini', undefined, 'expectedResult'],
+  // Complex output with complex config files
+  ['before2.json', 'after2.json', undefined, 'expectedResult2'],
+  ['before2.yaml', 'after2.yaml', undefined, 'expectedResult2'],
+  ['before2.ini', 'after2.ini', undefined, 'expectedResult2'],
+  // Plain output with flat config files
+  ['before.json', 'after.json', 'plain', 'expectedPlainResult'],
+  ['before.yaml', 'after.yaml', 'plain', 'expectedPlainResult'],
+  ['before.ini', 'after.ini', 'plain', 'expectedPlainResult'],
+  // Plain output with complex config files
+  ['before2.json', 'after2.json', 'plain', 'expectedPlainResult2'],
+  ['before2.yaml', 'after2.yaml', 'plain', 'expectedPlainResult2'],
+  ['before2.ini', 'after2.ini', 'plain', 'expectedPlainResult2'],
+  // JSON output with flat config files
+  ['before.json', 'after.json', 'json', 'expectedJSONResult'],
+  ['before.yaml', 'after.yaml', 'json', 'expectedJSONResult'],
+  ['before.ini', 'after.ini', 'json', 'expectedJSONResult'],
+  // JSON output with complex config files
+  ['before2.json', 'after2.json', 'json', 'expectedJSONResult2'],
+  ['before2.yaml', 'after2.yaml', 'json', 'expectedJSONResult2'],
+  ['before2.ini', 'after2.ini', 'json', 'expectedJSONResult2'],
 ];
 
-const testSet2 = [
-  ['before2.json', 'after2.json'],
-  ['before2.yaml', 'after2.yaml'],
-  ['before2.ini', 'after2.ini'],
-];
-
-describe('Complex output with flat config files', () => {
-  const expected = read('expectedResult');
-  test.each(testSet1)('genDiff(%s, %s)', (name1, name2) => {
-    testGenDiff(name1, name2, expected);
-  });
-});
-
-describe('Complex output with complex config files', () => {
-  const expected = read('expectedResult2');
-  test.each(testSet2)('genDiff(%s, %s)', (name1, name2) => {
-    testGenDiff(name1, name2, expected);
-  });
-});
-
-describe('Plain output with flat config files', () => {
-  const expected = read('expectedPlainResult');
-  test.each(testSet1)('genDiff(%s, %s, \'plain\')', (name1, name2) => {
-    testGenDiff(name1, name2, expected, 'plain');
-  });
-});
-
-describe('Plain output with complex config files', () => {
-  const expected = read('expectedPlainResult2');
-  test.each(testSet2)('genDiff(%s, %s, \'plain\')', (name1, name2) => {
-    testGenDiff(name1, name2, expected, 'plain');
-  });
-});
-
-describe('JSON output with flat config files', () => {
-  const expected = read('expectedJSONResult');
-  test.each(testSet1)('genDiff(%s, %s, \'json\')', (name1, name2) => {
-    testGenDiff(name1, name2, expected, 'json');
-  });
-});
-
-describe('JSON output with complex config files', () => {
-  const expected = read('expectedJSONResult2');
-  test.each(testSet2)('genDiff(%s, %s, \'json\')', (name1, name2) => {
-    testGenDiff(name1, name2, expected, 'json');
+describe('genDiff', () => {
+  test.each(testSet)('genDiff(%s, %s, %s)', (name1, name2, format, expectedName) => {
+    const expected = read(expectedName);
+    expect(genDiff(getPath(name1), getPath(name2), format)).toBe(expected);
   });
 });
